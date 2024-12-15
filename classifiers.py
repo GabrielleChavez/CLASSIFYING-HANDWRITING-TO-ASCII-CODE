fc7922ee40ded9df38ad951821e636dc636fedaa
Mehmet Emre Tiryaki <emre@mehmets-mbp.acasmart.jh.edu>
2024-12-15 18:42:25 -0500

    Merged models into classifiers.py

--- classifiers.py
+++ classifiers.py
@@ -1,36 +1,34 @@
 import numpy as np
 import xgboost as xgb
 import numpy as np
-from sklearn.metrics import f1_score, accuracy_score, confusion_matrix, precision_score, recall_score
-from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 
 # Fit and transform ytrain and ytest to sequential labels
-def xgboost_model(Xtrain, ytrain, n_estimators, max_depth):
+def train_xgboost(Xtrain, ytrain, n_estimators, max_depth):
    num_classes = len(np.unique(ytrain))
    model = xgb.XGBClassifier(objective='multi:softprob',num_class=num_classes,booster='gbtree',eval_metric= 'mlogloss', random_state=12,n_estimators= n_estimators, max_depth = max_depth)
    model.fit(Xtrain, ytrain)
    return model
-
-def xgboost_test(model, Xtest, ytest):
-   y_pred = model.predict(Xtest)
-   f1 = f1_score(ytest, y_pred, labels = range(0,94), average='maco')
-   acc = accuracy_score(ytest, y_pred)
-   cm = confusion_matrix(ytest, y_pred)
-   prec = precision_score(ytest, y_pred, labels = range(0,94), average = 'macro')
-   recall = recall_score(ytest, y_pred, labels = range(0,94), average = 'macro')
-   return acc, prec, recall, f1, cm
 
 def train_random_forest(x_train, y_train, n_estimators=50):
    clf = RandomForestClassifier(n_estimators=n_estimators)
    clf.fit(x_train, y_train)
    return clf
 
-def test_random_forest(clf, x_test, y_test):
-   y_pred = clf.predict(x_test)
-   accuracy = clf.score(x_test, y_test)
-   precision = precision_score(y_test, y_pred, average='macro')
-   recall = recall_score(y_test, y_pred, average='macro')
-   f1 = f1_score(y_test, y_pred, average='macro')
-   conf_matrix = confusion_matrix(y_test, y_pred)
-   return accuracy, precision, recall, f1, conf_matrix
+def train_perceptron(x_train, y_train):
+   perceptron = Perceptron(max_iter=100, random_state=42)
+   perceptron.fit(x_train, y_train)
+   return perceptron
+
+def train_svm(x_train, y_train, kernel='poly'):
+   svm_model = svm.SVC(kernel=kernel)
+   svm_model.fit(x_train, y_train)
+   return svm_model
+
+def train_knn(x_train, y_train, n_neighbors=3):
+   knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+   knn.fit(x_train, y_train)
+   return knn
