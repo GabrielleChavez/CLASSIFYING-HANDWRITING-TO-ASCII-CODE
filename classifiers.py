62a7ef3572de3d8799e961c1e861a851d336d6c9
gabrielle <gchavez5@jh.edu>
2024-12-15 19:07:49 -0500

    Added docstrings and changed parameter for percep

--- classifiers.py
+++ classifiers.py
@@ -6,29 +6,81 @@ from sklearn.linear_model import Perceptron
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 
-# Fit and transform ytrain and ytest to sequential labels
-def train_xgboost(Xtrain, ytrain, n_estimators, max_depth):
-   num_classes = len(np.unique(ytrain))
+def train_xgboost(X_train, y_train, n_estimators, max_depth):
+   """
+   Create and Train the xgboost model.
+
+   Parameters:
+      X_train (Numpy Array): training data
+      y_train (Numpy Array): training labels
+      n_estimators (int): number of trees the model will have
+      max_depth (int): limits the depth of each tree witihin the model
+   
+   Returns:
+      model: XGBClassifier model
+   """
+   num_classes = len(np.unique(y_train))
    model = xgb.XGBClassifier(objective='multi:softprob',num_class=num_classes,booster='gbtree',eval_metric= 'mlogloss', random_state=12,n_estimators= n_estimators, max_depth = max_depth)
-   model.fit(Xtrain, ytrain)
+   model.fit(X_train, y_train)
    return model
 
-def train_random_forest(x_train, y_train, n_estimators=50):
+def train_random_forest(X_train, y_train, n_estimators=50):
+   """
+   Create and Train RF model
+
+   Parameters:
+      X_train (Numpy Array): training data
+      y_train (Numpy Array): training labels
+      n_estimators (int): number of decision trees the model will use
+   
+   Returns:
+      clf: RandomForest Classfier model
+   """
    clf = RandomForestClassifier(n_estimators=n_estimators)
-   clf.fit(x_train, y_train)
+   clf.fit(X_train, y_train)
    return clf
 
-def train_perceptron(x_train, y_train):
-   perceptron = Perceptron(max_iter=100, random_state=42)
-   perceptron.fit(x_train, y_train)
+def train_perceptron(X_train, y_train, max_iter=100):
+   """
+   Create and Train perceptron model
+
+   Parameters:
+      X_train (Numpy Array): training data
+      y_train (Numpy Array): training labels
+      max_iter (int): max number of iterations over training data
+   Returns:
+      perceptron: perceptron model
+   """
+   perceptron = Perceptron(max_iter=max_iter, random_state=42)
+   perceptron.fit(X_train, y_train)
    return perceptron
 
-def train_svm(x_train, y_train, kernel='poly'):
+def train_svm(X_train, y_train, kernel='poly'):
+   """
+   Create and Train svm model
+
+   Parameters:
+      X_train (Numpy Array): training data
+      y_train (Numpy Array): training labels
+      kernal (String): classification type
+   Returns:
+      perceptron: perceptron model
+   """
    svm_model = svm.SVC(kernel=kernel)
-   svm_model.fit(x_train, y_train)
+   svm_model.fit(X_train, y_train)
    return svm_model
 
-def train_knn(x_train, y_train, n_neighbors=3):
+def train_knn(X_train, y_train, n_neighbors=94):
+   """
+   Create and Train knn model
+
+   Parameters:
+      X_train (Numpy Array): training data
+      y_train (Numpy Array): training labels
+      n_neighbors (int): number of classes
+   Returns:
+      knn: knn model
+   """
    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
-   knn.fit(x_train, y_train)
+   knn.fit(X_train, y_train)
    return knn
